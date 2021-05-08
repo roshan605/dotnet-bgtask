bf4f7012168a2d08ecf2a3aad2ef90f3e20bbaae
roshan <roshan605@gmail.com>
2021-05-08 14:14:10 +0530

    Adding migration

--- bg-task-api/Jubilant.API/Startup.cs
+++ bg-task-api/Jubilant.API/Startup.cs
@@ -1,7 +1,9 @@
+using Jubilant.API.Contexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,10 @@ namespace Jubilant.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            var connectionString = Configuration["ConnectionStrings:BooksDbConnectionString"];
+
+            services.AddDbContext<BookContext>(o => o.UseSqlServer(connectionString));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
