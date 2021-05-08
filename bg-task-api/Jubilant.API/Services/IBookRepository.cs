f02edb9664b881432d8e605cf4b967f6e682221d
roshan <roshan605@gmail.com>
2021-05-08 14:20:12 +0530

    Adding BookRepository

---
+++ bg-task-api/Jubilant.API/Services/IBookRepository.cs
@@ -0,0 +1,15 @@
+﻿using Jubilant.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jubilant.API.Services
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> GetBooksAsync();
+
+        Task<Book> GetBookAsync(Guid id);
+    }
+}
